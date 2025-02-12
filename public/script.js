c2b5d9933b89e0c284644cccc00396e8bb50ac30
Laura-Jarventie <laurajarventie@hotmail.com>
2025-02-12 16:56:33 +0200

    11.2

--- public/script.js
+++ public/script.js
@@ -1,13 +1,21 @@
 let BOARD_SIZE = 15;
 let board; //kenttä talennetaan tähän
-
-
-
-
-
+const cellSize = calculateCellSize();
 
 
 document.getElementById("new-game-btn").addEventListener('click', startGame);
+
+//JÄÄTIIN TÄHÄN ELI RESPOSIIN
+function calculateCellSize(){
+// Otetaan talteen pienempi luku ikkunan leveydestä ja korkeudesta
+  const screenSize = Math.min(window.innerWidth, window.innerHeight);
+
+  // Tehdään pelilaudasta hieman tätä pienempi, jotta jää pienet reunat
+  const gameBoardSize = 0.95 * screenSize;
+
+  // Laudan koko jaetaan ruutujen määrällä, jolloin saadaan yhden ruudun koko
+  return gameBoardSize / BOARD_SIZE;
+}
 
 function startGame(){
     document.getElementById("intro-screen").style.display = 'none';
@@ -15,6 +23,10 @@ function startGame(){
 
     board = generateRandomBoard();
     drawBoard(board);
+}
+
+function getCell(board, x, y) {
+    return board[y][x];
 }
 
 function generateRandomBoard(){
@@ -42,6 +54,23 @@ for (let y = 0; y < BOARD_SIZE; y++) {
 function drawBoard(board) {
     const gameBoard = document.getElementById('game-board');
 
-    //JÄÄTIIN TÄHÄN: Asetataan grid sarakkeet ja rivit dynaamisesti BOARD_SIZEN mukaan
+    //Asetataan grid sarakkeet ja rivit dynaamisesti BOARD_SIZEN mukaan
     gameBoard.style.gridTemplateColumns = `repeat(${BOARD_SIZE}, 1fr)`;
+
+//luodaan jokainen ruutu
+for (let y = 0; y< BOARD_SIZE; y++){
+    for (let x= 0; x < BOARD_SIZE; x++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell'); 
+
+        if (getCell(board, x, y) === 'W') {
+            cell.classList.add('wall')
+        }
+
+        gameBoard.appendChild(cell);
+        
+    }
 }
+
+}
+
